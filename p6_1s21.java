ddacd78bd9a9cf8dbead1f2418378778acffe46b
Jose Fernando Cruz Hernandez <144178844+JosefernandoCH@users.noreply.github.com>
2023-09-12 15:57:46 -0600

    practica 5 y 6 del 12 de septiembre

---
+++ p6_1s21.java
@@ -0,0 +1,12 @@
+class p6_1s21{
+    public static void main(String[] args){
+    String nombre="Fernando";
+    int edad=19;
+
+    System.out.println("El valor para edad es "+ nombre);
+    System.out.println("la edad es "+ edad);
+    System.out.println("puede conseguir programando");
+
+    }
+
+}
