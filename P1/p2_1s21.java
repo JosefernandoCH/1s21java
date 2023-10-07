33ae1f1ab7c4d122d85a9db4d6b5023cb7a65d13
Jose Fernando Cruz Hernandez <144178844+JosefernandoCH@users.noreply.github.com>
2023-10-06 23:15:13 -0600

    Add files via upload

---
+++ P1/p2_1s21.java
@@ -0,0 +1,11 @@
+class p2_1s21{
+public static void main(String[] args) {
+	System.out.println("Jose Fernando Cruz Hernandez");
+	System.out.println("18 años");
+	System.out.println("js152587@gmail.com");
+	System.out.println("basketball");
+	System.out.println("negro y rojo");
+
+
+}
+}
