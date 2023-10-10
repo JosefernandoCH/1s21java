2c9ccb9ef804102ce322cece217f4cfbee4b98ac
Jose Fernando Cruz Hernandez <144178844+JosefernandoCH@users.noreply.github.com>
2023-10-10 14:58:51 -0600

    Add files via upload

---
+++ P2/P2p3_1s21.java
@@ -0,0 +1,15 @@
+public class P2p3_1s21{
+	public static void main(String[] args){
+		for (int i=0;i <= 10;i ++){
+			System.out.println("tabla de multiplicar del numero" + i + ":");
+			for (int j = 0;j <= 10;j++) {
+			int resultado = i * j;
+			System.out.println(i + "X" + j + "=" + resultado);
+		    
+		    }
+		    System.out.println();
+		
+
+	    }
+	}
+}
